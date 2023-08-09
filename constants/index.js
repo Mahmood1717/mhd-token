bca175cb18cecf97f2c5688fc616913ac78658a5
Mahmood1717 <91190009+Mahmood1717@users.noreply.github.com>
2023-08-09 19:47:22 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xd3d3dbfd46ce426c7fabd755fcbd19e3053bc49c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
